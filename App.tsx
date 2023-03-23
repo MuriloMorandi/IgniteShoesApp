544d2273e0b0ffba77df85a4761daf520eb5e4fc
Murilo Morandi Alexandre <murilomorandialexandre2011@gmail.com>
2023-03-22 23:37:24 -0300

    Configurado o OneSignal

--- App.tsx
+++ App.tsx
@@ -2,15 +2,22 @@ import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import OneSignal from 'react-native-onesignal';
+
+OneSignal.setAppId("33bc7468-7cd7-416c-a71c-59fc797e93d3");
+
 import { Routes } from './src/routes';
 
 import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+import { tagUserEmailCreate } from './src/notifications/notificationsTahs';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  tagUserEmailCreate("teste@teste.com")
 
   return (
     <NativeBaseProvider theme={THEME}>
